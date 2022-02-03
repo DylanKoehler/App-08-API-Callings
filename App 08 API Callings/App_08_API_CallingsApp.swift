61b4d4207bbc34416e84221f08edec6a9c84fd58
Dylan Koehler <Dylankoehler3212@gmail.com>
2022-02-03 10:52:15 -0600

    Initial Commit

---
+++ App 08 API Callings/App_08_API_CallingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_08_API_CallingsApp.swift
+//  App 08 API Callings
+//
+//  Created by Student on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct App_08_API_CallingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
